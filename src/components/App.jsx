b3a87e9f473e36c0250e4e1314120ed86a030478
Paweł Witecki-Wawrzyniak <pawel.witecki.wawrzyniak@gmail.com>
2023-09-24 01:52:17 +0200

    Fixed everything, added css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,66 @@
-import { Section } from './Section';
-import { HandleStats } from './HandleStatistics';
+import { Section } from './Section/Section';
+import { Buttons } from './Buttons/Buttons';
+import { Statistics } from './Statistics/Statistics';
+import { Component } from 'react';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
+export class App extends Component {
+  static defaultProps = {
+    step: 1,
+  };
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleClickGood = () => {
+    this.setState((state, props) => ({
+      good: state.good + props.step,
+    }));
+  };
+
+  handleClickNeutral = () => {
+    this.setState((state, props) => ({
+      neutral: state.neutral + props.step,
+    }));
+  };
+
+  handleClickBad = () => {
+    this.setState((state, props) => ({
+      bad: state.bad + props.step,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const total = this.state.good + this.state.neutral + this.state.bad;
+    return total;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.state.good + this.state.neutral + this.state.bad;
+    const positivePercentage = (this.state.good / total) * 100;
+    return positivePercentage.toFixed(0);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
       <Section title="Please leave feedback">
-        <HandleStats></HandleStats>
+        <Buttons
+          handleGood={this.handleClickGood}
+          handleNeutral={this.handleClickNeutral}
+          handleBad={this.handleClickBad}
+        ></Buttons>
+        <h2>Statistics</h2>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={this.countTotalFeedback()}
+          positivePercentage={this.countPositiveFeedbackPercentage()}
+        ></Statistics>
       </Section>
-    </div>
-  );
-};
+    );
+  }
+}
